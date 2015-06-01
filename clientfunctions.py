aabcf7ccd573ecfd6e74a9b846fe6d97c9e3cd4e
Don <donreinertjr@gmail.com>
2015-06-01 01:51:28 -0500

    connection works in the fact that that it literally works, nothing more.

--- clientfunctions.py
+++ clientfunctions.py
@@ -1,6 +1,16 @@
 __author__ = 'Don'
 
+import socket
+
+
 
 # connection function
-
-
+def connect(target_server, port):
+    s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    socketaddrraw = socket.getaddrinfo(target_server, port)
+    socketaddrtuple = socketaddrraw[0]
+    socketaddr = socketaddrtuple[4]
+    try:
+        s.connect(socketaddr)
+    except:
+        print 'fuck errors'  #this is why you sanitize source code
--- clientmain.py
+++ clientmain.py
@@ -2,3 +2,7 @@ __author__ = 'Don'
 
 import clientfunctions
 
+host = raw_input('What is the host? ')
+port = raw_input('What is the port? ')
+
+clientfunctions.connect(host, port)
